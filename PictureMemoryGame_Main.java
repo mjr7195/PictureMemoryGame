823e741a6db5795264331247eb5ab847c581292b
Marcos <mjr7195@psu.edu>
2023-10-12 18:32:06 -0400

    Finished

--- PictureMemoryGame_Main.java
+++ PictureMemoryGame_Main.java
@@ -2,29 +2,49 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.Instant;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.time.Duration;
 
 
 public class PictureMemoryGame_Main extends JFrame {
+    Instant startTime = Instant.now();
     private ArrayList<String> imagePaths;
     private ArrayList<String> cardImages;
     private JButton[] cardButtons;
     private String[] imageNames = {"Photo1.JPG","Photo2.JPG", "Photo3.JPG", "Photo4.JPG", "Photo5.jpg", "Photo6.jpg", "Photo7.jpg", "Photo8.jpg", "Photo9.jpg", "Photo10.jpg", "Photo11.jpg", "Photo12.jpg",
                                    "Photo1.JPG","Photo2.JPG", "Photo3.JPG", "Photo4.JPG", "Photo5.jpg", "Photo6.jpg", "Photo7.jpg", "Photo8.jpg", "Photo9.jpg", "Photo10.jpg", "Photo11.jpg", "Photo12.jpg"};
+    private String[] image6Names = {"Photo1.JPG","Photo2.JPG", "Photo3.JPG", "Photo4.JPG", "Photo5.jpg", "Photo6.jpg",
+                                    "Photo1.JPG","Photo2.JPG", "Photo3.JPG", "Photo4.JPG", "Photo5.jpg", "Photo6.jpg"};
     private int numberOfMatches;
     private int firstCardIndex = -1;
     private int secondCardIndex;
 
+    JFrame frame = new JFrame();
+
     public PictureMemoryGame_Main(){
         setTitle("Picture Memory Game");
-        setSize(400, 600);
+        setSize(400, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         imagePaths = new ArrayList<>();
-        // add file paths to images
-        imagePaths.addAll(Arrays.asList(imageNames));
+
+        String cardNum = JOptionPane.showInputDialog("How many cards do you want to play with?");
+        int cardNumber = Integer.parseInt(cardNum);
+        while(cardNumber != 24 && cardNumber != 12) {
+            cardNum = JOptionPane.showInputDialog("Invalid card amount. Please enter 12 or 24 cards?");
+            cardNumber = Integer.parseInt(cardNum);
+        }
+        if (cardNumber == 24) {
+            // add file paths to images
+            imagePaths.addAll(Arrays.asList(imageNames));
+        }
+        if (cardNumber == 12) {
+            imagePaths.addAll(Arrays.asList(image6Names));
+        }
 
         cardImages = new ArrayList<>();
         for (String imagePath : imagePaths) {
@@ -34,8 +54,8 @@ public class PictureMemoryGame_Main extends JFrame {
         Collections.shuffle(imagePaths);
         Collections.shuffle(cardImages);
 
-        JPanel cardPanel = new JPanel(new GridLayout(12,12));
-        cardButtons = new JButton[24];
+        JPanel cardPanel = new JPanel(new GridLayout(5,5));
+        cardButtons = new JButton[cardNumber];
 
         for (int i = 0; i < cardButtons.length; i++) {
             final int index = i;
@@ -53,6 +73,7 @@ public class PictureMemoryGame_Main extends JFrame {
     }
 
     private void handleCardClick(int index) {
+        Instant startTime = Instant.now();
         if (cardButtons[index].getIcon() == null) {
             return;
         }
@@ -76,6 +97,7 @@ public class PictureMemoryGame_Main extends JFrame {
 
                         if (numberOfMatches == imagePaths.size() / 2) {
                             JOptionPane.showMessageDialog(null, "Congrats! You Won!!!");
+                            JOptionPane.showMessageDialog(null, "You took "+ timeTaken +" seconds to complete game");
                             System.exit(0);
                         }
                     } else {
@@ -89,8 +111,8 @@ public class PictureMemoryGame_Main extends JFrame {
             timer.start();
         }
     }
-
     public static void main(String[] args) {
+
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
@@ -98,7 +120,7 @@ public class PictureMemoryGame_Main extends JFrame {
             }
         });
     }
-
-
+    Instant endTime = Instant.now();
+    Duration timeTaken = Duration.between(startTime,endTime);
 
 }
